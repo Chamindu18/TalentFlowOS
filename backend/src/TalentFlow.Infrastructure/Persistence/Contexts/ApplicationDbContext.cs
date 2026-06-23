1d470e59341aeb3c34d5eee621852399706ee0f1
Nethsara <hhnvinduwara@students.nsbm.ac.lk>
2026-06-23 22:09:31 +0530

    feat(infrastructure): add application db context

---
+++ backend/src/TalentFlow.Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using TalentFlow.Domain.Entities;
+
+namespace TalentFlow.Infrastructure.Persistence.Contexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(ApplicationDbContext).Assembly);
+    }
+}
