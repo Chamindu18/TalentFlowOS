67b4b14ba51f86124b0650b0d5e479b6cc0d10f5
Chamindu18 <rachranasinghe@students.nsbm.ac.lk>
2026-06-23 20:28:48 +0530

    feat(auth): add register request validator

---
+++ backend/src/TalentFlow.Application/Validators/Auth/RegisterRequestValidator.cs
@@ -0,0 +1,26 @@
+using FluentValidation;
+using TalentFlow.Application.DTOs.Auth;
+
+namespace TalentFlow.Application.Validators.Auth;
+
+public class RegisterRequestValidator : AbstractValidator<RegisterRequestDto>
+{
+    public RegisterRequestValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.LastName)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(8);
+    }
+}
