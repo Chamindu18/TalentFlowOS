257a941cd18e3ee8af5ac7bf4cec21f5b4468844
Chamindu18 <rachranasinghe@students.nsbm.ac.lk>
2026-06-23 20:37:14 +0530

    feat(auth): add login request validator

---
+++ backend/src/TalentFlow.Application/Validators/Auth/LoginRequestValidator.cs
@@ -0,0 +1,17 @@
+using FluentValidation;
+using TalentFlow.Application.DTOs.Auth;
+
+namespace TalentFlow.Application.Validators.Auth;
+
+public class LoginRequestValidator : AbstractValidator<LoginRequestDto>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty();
+    }
+}
