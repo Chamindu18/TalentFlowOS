6f202cf65dcdc51fa60fc47d36f47281d88f9af6
Chami-Ishu <ishiishi0307@gmail.com>
2026-06-26 13:52:03 +0530

    feat(application): add repository and service interfaces for jobs and applications module
    - Add IJobRepository interface
    - Add IApplicationRepository interface
    - Add ICompanyRepository interface
    - Add IDepartmentRepository interface
    - Add IJobService interface
    - Add IApplicationService interface

---
+++ backend/src/TalentFlow.Application/Interfaces/Repositories/IApplicationRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using TalentFlow.Domain.Entities;
+
+namespace TalentFlow.Application.Interfaces.Repositories;
+
+public interface IApplicationRepository
+{
+    Task<Application?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Application>> GetByCandidateIdAsync(Guid candidateId);
+    Task<IEnumerable<Application>> GetByJobIdAsync(Guid jobId);
+    Task<IEnumerable<Application>> GetByStatusAsync(string status);
+    Task<IEnumerable<Application>> GetApplicationsWithDetailsAsync();
+    Task AddAsync(Application application);
+    void Update(Application application);
+    void Delete(Application application);
+    Task<bool> HasCandidateAppliedAsync(Guid candidateId, Guid jobId);
+    Task<int> GetApplicationCountForJobAsync(Guid jobId);
+}
---
+++ backend/src/TalentFlow.Application/Interfaces/Services/IApplicationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using TalentFlow.Application.DTOs.Applications;
+
+namespace TalentFlow.Application.Interfaces.Services;
+
+public interface IApplicationService
+{
+    Task<ApplicationResponseDTO> GetByIdAsync(Guid id);
+    Task<IEnumerable<ApplicationResponseDTO>> GetByCandidateIdAsync(Guid candidateId);
+    Task<IEnumerable<ApplicationResponseDTO>> GetByJobIdAsync(Guid jobId);
+    Task<IEnumerable<ApplicationResponseDTO>> GetAllAsync();
+    Task<ApplicationResponseDTO> CreateAsync(CreateApplicationRequestDTO request);
+    Task<ApplicationResponseDTO> UpdateStatusAsync(Guid id, UpdateApplicationStatusRequestDTO request);
+    Task DeleteAsync(Guid id);
+    Task<bool> HasCandidateAppliedAsync(Guid candidateId, Guid jobId);
+}
