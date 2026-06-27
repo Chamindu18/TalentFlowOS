2078512d0195224b9ab3074a3e78718acdb93cd3
Nethsara <hhnvinduwara@students.nsbm.ac.lk>
2026-06-27 16:58:24 +0530

    feat(infrastructure): add entity configurations and migrations for jobs and applications module
    - Add CompanyConfiguration
    - Add DepartmentConfiguration
    - Add JobConfiguration
    - Add JobApplicationConfiguration
    - Update ApplicationDbContext with new DbSets
    - Add migration for Jobs and Applications tables

---
+++ backend/src/TalentFlow.Infrastructure/Configurations/CompanyConfiguration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TalentFlow.Domain.Entities;
+
+namespace TalentFlow.Infrastructure.Configurations;
+
+public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+{
+    public void Configure(EntityTypeBuilder<Company> builder)
+    {
+        builder.ToTable("Companies");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        builder.Property(x => x.Industry)
+            .HasMaxLength(100);
+
+        builder.Property(x => x.WebsiteUrl)
+            .HasMaxLength(500);
+
+        builder.Property(x => x.Address)
+            .HasMaxLength(255);
+
+        builder.Property(x => x.City)
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Country)
+            .HasMaxLength(100);
+
+        // Relationships
+        builder.HasMany(x => x.Departments)
+            .WithOne(x => x.Company)
+            .HasForeignKey(x => x.CompanyId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(x => x.Jobs)
+            .WithOne(x => x.Company)
+            .HasForeignKey(x => x.CompanyId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Indexes
+        builder.HasIndex(x => x.Name);
+        builder.HasIndex(x => x.Industry);
+    }
+}
---
+++ backend/src/TalentFlow.Infrastructure/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TalentFlow.Domain.Entities;
+
+namespace TalentFlow.Infrastructure.Configurations;
+
+public class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+{
+    public void Configure(EntityTypeBuilder<Department> builder)
+    {
+        builder.ToTable("Departments");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(500);
+
+        // Relationships
+        builder.HasOne(x => x.Company)
+            .WithMany(x => x.Departments)
+            .HasForeignKey(x => x.CompanyId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(x => x.Jobs)
+            .WithOne(x => x.Department)
+            .HasForeignKey(x => x.DepartmentId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Indexes
+        builder.HasIndex(x => x.CompanyId);
+        builder.HasIndex(x => new { x.CompanyId, x.Name })
+            .IsUnique();
+    }
+}
