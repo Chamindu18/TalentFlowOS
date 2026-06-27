68e035b44c0d184059f01d38094356efc079e569
Nethsara <hhnvinduwara@students.nsbm.ac.lk>
2026-06-27 11:42:51 +0530

    feat(application): add validators for jobs and applications module
    - Add CreateJobRequestValidator
    - Add UpdateJobRequestValidator
    - Add CreateApplicationRequestValidator
    - Add UpdateApplicationStatusRequestValidator

---
+++ backend/src/TalentFlow.Application/Validators/Applications/UpdateApplicationStatusRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentValidation;
+using TalentFlow.Application.DTOs.Applications;
+using TalentFlow.Domain.Enums;
+
+namespace TalentFlow.Application.Validators.Applications;
+
+public class UpdateApplicationStatusRequestValidator : AbstractValidator<UpdateApplicationStatusRequestDTO>
+{
+    public UpdateApplicationStatusRequestValidator()
+    {
+        RuleFor(x => x.Status)
+            .NotEmpty().WithMessage("Status is required")
+            .Must(BeValidStatus).WithMessage($"Status must be one of: {string.Join(", ", Enum.GetNames(typeof(ApplicationStatus)))}");
+    }
+
+    private bool BeValidStatus(string status)
+    {
+        return Enum.TryParse<ApplicationStatus>(status, true, out _);
+    }
+}
