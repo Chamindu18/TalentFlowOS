60adc37fedf8e1bf5b51b81425b3b416301f89de
SahanRathnaweera <sahantharuka0909@gmail.com>
2026-07-07 11:39:03 +0530

    Merge develop and save unit tests

--- backend/src/TalentFlow.Application/Services/CandidateService.cs
+++ backend/src/TalentFlow.Application/Services/CandidateService.cs
@@ -1,48 +1,44 @@
 using System;
 using System.Threading.Tasks;
-using AutoMapper;
-
 using TalentFlow.Application.DTOs.Candidate;
 using TalentFlow.Application.Interfaces.Services;
+using TalentFlow.Application.Interfaces.Repositories;
 using TalentFlow.Domain.Entities;
 
 namespace TalentFlow.Application.Services
 {
     public class CandidateService : ICandidateService
     {
+        private readonly ICandidateRepository _repository;
 
-
-        public CandidateService()
+        public CandidateService(ICandidateRepository repository)
         {
-           
+            _repository = repository;
         }
 
-        public async Task<CandidateProfileDto?> GetProfileByUserIdAsync(string userId)
-        {
-            
-            return new CandidateProfileDto
-            {
-                Id = Guid.NewGuid(),
-                FirstName = "Test",
-                LastName = "Candidate",
-                Phone = "0771234567",
-                Bio = "Software Engineering Student",
-                IsProfileComplete = true
-            };
-        }
+        public async Task<CandidateProfileDto?> GetProfileByUserIdAsync(string userId) => null;
 
-        public async Task<CandidateProfileDto> UpdateProfileAsync(string userId, UpdateCandidateProfileDto dto)
+        public async Task<CandidateProfileDto> UpdateProfileAsync(string userId, UpdateCandidateProfileDto dto) => new CandidateProfileDto();
+
+        public async Task<bool> AddEducationAsync(string userId, EducationDto educationDto)
         {
-            
-            return new CandidateProfileDto
+            var candidate = await _repository.GetCandidateByUserIdAsync(userId);
+            if (candidate == null) return false;
+
+            var education = new Education
             {
-                Id = Guid.NewGuid(),
-                FirstName = dto.FirstName,
-                LastName = dto.LastName,
-                Phone = dto.Phone,
-                Bio = dto.Bio,
-                IsProfileComplete = true
+                Institution = educationDto.Institution,
+                Degree = educationDto.Degree,
+                FieldOfStudy = educationDto.FieldOfStudy,
+                StartDate = educationDto.StartDate,
+                EndDate = educationDto.EndDate,
+                Gpa = educationDto.Gpa,
+                CandidateId = candidate.Id
             };
+
+            
+            candidate.Educations.Add(education); 
+            return await _repository.SaveChangesAsync();
         }
     }
 }
