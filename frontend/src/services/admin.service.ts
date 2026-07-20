03ed5517a532e8a3b52850f9a455e81bb19c1f90
Nethsara <Nethsara@gmail.com>
2026-07-21 00:55:44 +0530

    feat(admin): connect user management with database

---
+++ frontend/src/services/admin.service.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5007/api";
+
+export const adminService = {
+  async getUsers() {
+    const response = await axios.get(
+      `${API_URL}/admin/users`
+    );
+
+    return response.data.data;
+  },
+};
