e6399f0b8e45de84be178bd05d5d9f6981ef8938
Chamindu18 <rachranasinghe@students.nsbm.ac.lk>
2026-09-14 17:08:40 +0530

    fix: map application candidate names

--- backend/src/TalentFlow.Application/Mappings/JobApplicationProfile.cs
+++ backend/src/TalentFlow.Application/Mappings/JobApplicationProfile.cs
@@ -18,7 +18,7 @@ public class JobApplicationProfile : Profile
         CreateMap<JobApplication, ApplicationResponseDTO>()
             .ForMember(dest => dest.JobTitle, opt => opt.MapFrom(src => src.Job != null ? src.Job.Title : string.Empty))
             .ForMember(dest => dest.CompanyName, opt => opt.MapFrom(src => src.Job != null && src.Job.Company != null ? src.Job.Company.Name : string.Empty))
-            .ForMember(dest => dest.CandidateName, opt => opt.MapFrom(src => "Pending")); // Will be updated when Candidate module is ready
+            .ForMember(dest => dest.CandidateName, opt => opt.MapFrom(src => src.Candidate != null ? $"{src.Candidate.FirstName} {src.Candidate.LastName}" : "Unknown"));
 
         // DTO to Entity
         CreateMap<CreateApplicationRequestDTO, JobApplication>();
